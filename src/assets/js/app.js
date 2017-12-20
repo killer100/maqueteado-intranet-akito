ce15f0a2e3880bc7dd77a69bd0cf1055ffd386ec
Mario Garcia Chihuanhuaylla <mgarciach@produce.gob.pe>
2017-12-20 17:54:51 -0500

    third commit

---
+++ src/assets/js/app.js
@@ -0,0 +1,40 @@
+(function () {
+    angular.module('app', ['ui.router'])
+        .config(function ($stateProvider, $urlRouterProvider) {
+            var homeState = {
+                name: 'home',
+                url: '/',
+                templateUrl: 'pages/home.html'
+            }
+            var documentosNotmativosState = {
+                name: 'documentos-normativos',
+                url: '/documentos-normativos',
+                templateUrl: 'pages/documentos-normativos.html'
+            }
+
+            var areasState = {
+                name: 'areas',
+                url: '/areas',
+                templateUrl: 'pages/areas.html'
+            }
+
+            var comitesCajaState = {
+                name: 'comites-cajas',
+                url: '/comites-cajas',
+                templateUrl: 'pages/comites-cajas.html'
+            }
+
+            var sistemasGestionState = {
+                name: 'sistemas-gestion',
+                url: '/sistemas-gestion',
+                templateUrl: 'pages/sistemas-gestion.html'
+            }
+
+            $stateProvider.state(homeState);
+            $stateProvider.state(documentosNotmativosState);
+            $stateProvider.state(areasState);
+            $stateProvider.state(comitesCajaState);
+            $stateProvider.state(sistemasGestionState);
+            $urlRouterProvider.otherwise("/");
+        });
+})();
